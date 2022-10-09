ea5c290ac96b66fe8ac91c623b0a863f77605b88
Shubhangi Goyal <36481884+goyalshubhangi@users.noreply.github.com>
2022-10-09 19:36:10 +0530

    Create airplane-seat-assignment.cpp

---
+++ cpp/airplane-seat-assignment.cpp
@@ -0,0 +1,23 @@
+// Time:  O(1)
+// Space: O(1)
+
+class Solution {
+public:
+    double nthPersonGetsNthSeat(int n) {
+        return n != 1 ? 0.5 : 1.0;
+    }
+};
+
+// Time:  O(n)
+// Space: O(1)
+class Solution2 {
+public:
+    double nthPersonGetsNthSeat(int n) {
+        vector<double> dp(2);
+        dp[0] = 1.0;  // zero-indexed
+        for (int i = 2; i <= n; ++i) {
+            dp[(i - 1) % 2] = 1.0 / i + dp[(i - 2) % 2] * (i - 2) / i;
+        }
+        return dp[(n - 1) % 2];
+    }
+};
